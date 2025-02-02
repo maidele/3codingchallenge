1a4acb43c3c9dff608e9d8b7bc73ac390ca08fb7
Maidele Archelus <maidele@usf.edu>
2025-02-02 03:05:39 -0500

    Task 2

--- cc_3.js
+++ cc_3.js
@@ -1,6 +1,15 @@
 
-let price = ["$20", "$25", "$30", "$35", "$40"];
+let price = ["$20", "$25", "$30", "$35", "$40"]; 
 price.push("71");
 price.pop();
 console.log("Updated Product List:", products);
 
+//Task 2
+let orders = [100,200,300,400,500];// Order array is declared
+
+orders[2]+=5; // The third quantity within the order is being increased by 5 here
+
+let totalOrders = orders.reduce((total, amount) => total + amount, 0);
+
+console.log("Orders with their Updates:", orders)
+console.log(`Orders with their Updates': $${totalOrders}`);  // Output: Total amount of orders: $1500 !!
